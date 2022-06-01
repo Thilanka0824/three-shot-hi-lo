6a6a58093ad580d65e4d66a86bbdcdc789cc730f
Thilanka0824 <frontleanrest24@gmail.com>
2022-06-01 09:35:35 -0700

    completed

---
+++ main.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+console.log("I'm thinking of a number between 1 and 100. Try to guess it.")
+let n = Number(prompt(">"));
+
+let myNum = Math.ceil(Math.random() * 100)
+
+let count = 0
+while (count < 2) {
+
+    if (n === myNum) {
+        console.log("Congratulations, " + n + " is correct")
+    } else if (n > myNum) {
+        console.log("Sorry, too high! Guess again.")
+        n = Number(prompt(">"))
+    } else if (n < myNum) {
+        console.log("Sorry, too low! Guess again.")
+        n = Number(prompt(">"))
+    }
+    
+    count = count + 1
+} 
+
+if (n > myNum) {
+    console.log("Sorry too high. I was thinking " + myNum)
+} else if (n < myNum) {
+    console.log("Sorry too low. I was thinking " + myNum)
+}

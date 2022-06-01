43a12967735c4213f6dfe82bdbc253e8a448d961
Thilanka0824 <frontleanrest24@gmail.com>
2022-06-01 11:06:02 -0700

    completed

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const prompt = require('prompt-sync')({ sigint: true });
 console.log("I'm thinking of a number between 1 and 100. Try to guess it.")
 let n = Number(prompt(">"));
 
-let myNum = Math.ceil(Math.random() * 100)
+let myNum = Math.ceil(Math.random() * 2)
 
 let count = 0
-while (count < 2) {
+while (count < 2 && n !== myNum) {
 
     if (n === myNum) {
         console.log("Congratulations, " + n + " is correct")
@@ -21,7 +21,9 @@ while (count < 2) {
     count = count + 1
 } 
 
-if (n > myNum) {
+if (n === myNum) {
+    console.log("Congratulations, " + n + " is correct")
+} else if (n > myNum) {
     console.log("Sorry too high. I was thinking " + myNum)
 } else if (n < myNum) {
     console.log("Sorry too low. I was thinking " + myNum)
